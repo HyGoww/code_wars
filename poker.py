d0c66f289da68f268ca54440f20d43985654d169
HyGoww <hygoww@gmail.com>
2025-09-15 16:52:06 +0200

    poker

--- poker.py
+++ poker.py
@@ -6,3 +6,21 @@
 
 # The possible card values are 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A
 
+realFlush = ["AS", "3S", "9S", "KS", "4S"]
+fakeFlush = ["AD", "4S", "7H", "KS", "10S"]
+
+def all_letters_are_same(string):
+    # fonction all qui permet en fonction du premier caractère voir si les autres sont pareils
+    return all(s == string[0] for s in string)
+
+def flush(suite):
+    letters = ""
+    for card in suite:
+        letters += card[-1]
+    if(all_letters_are_same(letters)):
+        print("Flush !")
+    else:
+        print("Not flush looser booooouuuuuuh le gros naze t'as tout perdu")
+        
+
+flush(fakeFlush)
